d30aad674952f7bde62ac693706e6250dab9f226
dandongxue <240861544@qq.com>
2016-09-29 09:44:03 +0800

    add DisplayTree and modified the tree

--- Trie.cpp
+++ Trie.cpp
@@ -1,52 +1,52 @@
 #include<iostream>
 #include <set>
 using namespace std;
-struct tirenode{
-                set<struct tirenode> tnode_set;
+struct TrieNode{
+                set<struct TrieNode> tnode_set;
                 bool isStr;
-                int value;
-                int type;
+                int tnode_value;
+                int tnode_type;
 				};
-bool operator<(const tirenode & lhs, const tirenode & rhs)
+bool operator<(const TrieNode & lhs, const TrieNode & rhs)//over load operator '<' for set
 {
-    return lhs.value<rhs.value;
+    return lhs.tnode_value<rhs.tnode_value;
 }
-tirenode& newnode()
+TrieNode& CreateNode()
 {
-   static tirenode temp;
-   temp.type=-1;
-   temp.value=0;
-   temp.isStr=false;//标记位记录二叉树到这个节点是否构成了一个字符串，搜索是否需要终止
+   static TrieNode temp;
+   temp.tnode_type=-1;
+   temp.tnode_value=0;
+   temp.isStr=false;
    return temp;
 }
-tirenode& newnode(int value)
+TrieNode& CreateNode(int tnode_value)
 {
-   static tirenode temp;
-   temp.type=-1;
-   temp.value=value;
-   temp.isStr=false;//标记位记录二叉树到这个节点是否构成了一个字符串，搜索是否需要终止
+   static TrieNode temp;
+   temp.tnode_type=-1;
+   temp.tnode_value=tnode_value;
+   temp.isStr=false;
    return temp;
 }
 
-bool FindFromCurNode(tirenode *p,int id,tirenode *&tnode)
+bool FindFromCurNode(TrieNode *p,int id,TrieNode *&tnode)
 {
-   set<struct tirenode>::iterator iter;
+   set<struct TrieNode>::iterator iter;
    for(iter=p->tnode_set.begin();iter!=p->tnode_set.end();iter++)
    {
-       if((*iter).value == id)
+       if((*iter).tnode_value == id)
            {
-               tnode=const_cast<struct tirenode *>(&(*iter));
+               tnode=const_cast<struct TrieNode *>(&(*iter));
                return true;
            }
    }
     return false;
 }
-bool Search(tirenode &temp,char *ch)
+bool SearchNode(TrieNode &temp,char *ch)
 {
    bool Flag = false;
    int id;
-   tirenode *pTemp;
-   tirenode* p=&temp;
+   TrieNode *pTemp;
+   TrieNode* p=&temp;
    while(*ch)
    {
     id=*ch;
@@ -63,7 +63,7 @@ bool Search(tirenode &temp,char *ch)
     else
     {
         p=pTemp;
-        cout<<"search:"<<(char)p->value<<endl;
+        cout<<"search:"<<(char)p->tnode_value<<endl;
 
     }
        ch++;
@@ -71,22 +71,22 @@ bool Search(tirenode &temp,char *ch)
 return Flag;
 }
 
-void Insert(tirenode &temp,char *ch)
+void InsertNode(TrieNode &temp,char *ch)
 {
    int id;
-   set<struct tirenode>::iterator iter;
-  tirenode *p=&temp;
-   tirenode *tnode,tde;
+   set<struct TrieNode>::iterator iter;
+  TrieNode *p=&temp;
+   TrieNode *tnode,tde;
    while(*ch)
    {
         id=*ch;
         if(!FindFromCurNode(p,id,tnode))
         {
-            tde = newnode(id);
+            tde = CreateNode(id);
             (p->tnode_set).insert(tde);
             iter=(p->tnode_set).find(tde);
-            p = const_cast<struct tirenode *>(&(*iter));
-            cout<<"Insert "<<*ch<<endl;
+            p = const_cast<struct TrieNode *>(&(*iter));
+            cout<<"InsertNode "<<*ch<<endl;
            //Find it exit node
         }
         else
@@ -95,25 +95,29 @@ void Insert(tirenode &temp,char *ch)
    }
    p->isStr=true;
 }
-
-/*
-void Del(tirenode *p)
+void DisplayTree(const TrieNode *p,unsigned int curlevel) //Print the tree on the screen
 {
-  int i;
-  for(i=0;i<2;i++)
-  {
-    if(p->next[i]!=NULL)
-       Del(p->next[i]);
-  }
-free(p);
-}*/
+    //if(curlevel!=0)cout<<curlevel;
+    for(int i=0;i<curlevel;i++)
+        cout<<"-";
+    cout <<(char)p->tnode_value << " "<<endl;
+    set<struct TrieNode>::iterator iter;
+    for(iter=p->tnode_set.begin();iter!=p->tnode_set.end();iter++)
+    {
+        DisplayTree(&(*iter),curlevel+1);
+    }
+
+}
 int main(){
 	int count=1;
-	tirenode root=newnode();
-	Insert(root,"xkt");
-	Insert(root,"xkty");
+	TrieNode root=CreateNode();
+	InsertNode(root,"xdfdktt");
+	InsertNode(root,"xdfduyy");
+	InsertNode(root,"zbty");
+	InsertNode(root,"yjty");
 	cout<<"================="<<endl;
-    cout<<Search(root,"xktu")<<endl;
+    cout<<SearchNode(root,"xktu")<<endl;
+    DisplayTree(&root,0);
 	bool flag=true;
 	return 0;
 }
